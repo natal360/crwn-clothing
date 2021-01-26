348eb7725e48c0872911abca436e63571a5cb991
natal <drossel.fb@gmail.com>
2021-01-26 10:43:19 +0900

    added cartIcon and cartDropdown components to our application, also implemented cart reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data.js';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview'
+import CollectionPreview from '../../components/collection-preview/collection-preview';
 
 class ShopPage extends React.Component{
   constructor(props) {
@@ -10,7 +10,7 @@ class ShopPage extends React.Component{
 
     this.state = {
       collections: SHOP_DATA
-    }
+    };
   }
 
   render() {
@@ -19,10 +19,9 @@ class ShopPage extends React.Component{
     {
       collections.map(({ id, ...otherCollectionProps }) => (
         <CollectionPreview key={id} {...otherCollectionProps} />
-      ))
-    }
-      
-      </div>)
+      ))}
+      </div>
+    );
   }
 }
 
